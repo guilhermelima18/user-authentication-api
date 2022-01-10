73e0a51cbaf6fbaa42fd28741f9dc9763745e498
guilhermelima18 <guilhermelima18@hotmail.com>
2022-01-09 22:47:46 -0300

    Adicionado endpoint de validação de token

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import basicAuthentication from "./middlewares/basicAuthentication.middleware";
+import bearerAuthentication from "./middlewares/bearerAuthentication.middleware";
 import errorHandler from "./middlewares/errorHandler.middleware";
 import authorizationRoute from "./routes/authorization.route";
-import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
@@ -10,9 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(usersRoute);
-app.use(statusRoute);
 app.use(authorizationRoute);
+app.use(bearerAuthentication);
+app.use(usersRoute);
 
 app.use(errorHandler);
 
--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response, Router } from "express";
-import ForbiddenError from "../models/errors/forbiddenError.model";
-import userRepository from "../repositories/user.repository";
-import JWT from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
+import JWT from "jsonwebtoken";
 import basicAuthentication from "../middlewares/basicAuthentication.middleware";
+import bearerAuthentication from "../middlewares/bearerAuthentication.middleware";
+import ForbiddenError from "../models/errors/forbiddenError.model";
 
 const authorizationRoute = Router();
+
+authorizationRoute.post(
+  "/token/validate",
+  bearerAuthentication,
+  (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+  }
+);
 
 authorizationRoute.post(
   "/token",
